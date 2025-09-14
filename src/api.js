335442ab97f2707fb9a73d2b9fd6d0472d96bb60
oscar-ufmt <160333066+oscar-ufmt@users.noreply.github.com>
2025-09-14 15:24:48 -0400

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://10.26.8.128:8000/api', // endereço do seu backend Laravel
+    baseURL: 'http://localhost:8000/api', // endereço do seu backend Laravel
     headers: {
         'Content-Type': 'application/json'
     }
